77b401b6b41adcd0ac8b720a5b8c20a3908ad441
Chan-XOXO <singh.chanpreet.b@gmail.com>
2025-03-22 11:26:01 +0100

    change in index

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const { default: Channel } = require("./models/ChannelModel.js");
-const { default: Message } = require("./models/MessagesModel.js");
-const { default: User } = require("./models/UserModel.js");
+const Channel = require("./models/ChannelModel.js");
+const Message = require("./models/MessagesModel.js");
+const User = require("./models/UserModel.js");
 
 module.exports = {
-    User: require("./models/UserModel.js"),
-    Channel: require("./models/ChannelModel.js"),
-    Message: require("./models/MessagesModel.js")
-
-  };
+  User,
+  Channel,
+  Message
+};
